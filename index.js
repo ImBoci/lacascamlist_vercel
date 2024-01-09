367b1547eb9a272db85b08645e5f541a93028c26
ImBoci <picipycurka@gmail.com>
2024-01-09 16:33:06 +0100

    first commit

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-import express from "express";
-import bodyParser from "body-parser";
-import pg from "pg";
+// import express from "express";
+// import bodyParser from "body-parser";
+const express = require("express");
+const bodyParser = require("body-parser");
+// import pg from "pg";
+
+const postgre = require("./database");
 
 const app = express();
 const port = 3000;
 
-const db = new pg.Client({
-  user: process.env.USERNAME,
-  host: process.env.HOSTNAME,
-  database: process.env.DATABASE_NAME,
-  password: process.env.PASSWORD,
-  port: 5432,
-});
-db.connect();
+// const db = new pg.Client({
+//   user: process.env.USERNAME,
+//   host: process.env.HOSTNAME,
+//   database: process.env.DATABASE_NAME,
+//   password: process.env.PASSWORD,
+//   port: 5432,
+// });
+// db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 async function checkScams() {
-  const result = await db.query("SELECT * FROM scamlist ORDER BY id ASC");
+  const result = await postgre.query("SELECT * FROM scamlist ORDER BY id ASC");
   let scams = result.rows;
   console.log(result.rows);
   return scams;
@@ -38,7 +42,7 @@ app.post("/newscam", async (req, res) => {
   const description = req.body.newScamDescription;
 
   try {
-    await db.query(
+    await postgre.query(
       "INSERT INTO scamlist (scam_name, scam_description) VALUES ($1, $2)",
       [title, description]
     );
@@ -54,7 +58,7 @@ app.post("/editScam", async (req, res) => {
   const id = req.body.updatedItemId;
 
   try {
-    await db.query(
+    await postgre.query(
       "UPDATE scamlist SET scam_name = ($1), scam_description = ($2) WHERE id = $3",
       [title, description, id]
     );
@@ -67,7 +71,7 @@ app.post("/editScam", async (req, res) => {
 app.post("/deleteScam", async (req, res) => {
   const id = req.body.deleteItemId;
   try {
-    await db.query("DELETE FROM scamlist WHERE id = $1", [id]);
+    await postgre.query("DELETE FROM scamlist WHERE id = $1", [id]);
     res.redirect("/");
   } catch (err) {
     console.log(err);
@@ -75,5 +79,5 @@ app.post("/deleteScam", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
