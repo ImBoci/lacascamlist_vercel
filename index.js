2d4487a43daf5cc88569fb6b0fdd25b57e3e300f
ImBoci <picipycurka@gmail.com>
2024-01-09 17:01:46 +0100

    hotfix

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-// import express from "express";
-// import bodyParser from "body-parser";
-const express = require("express");
-const bodyParser = require("body-parser");
-// import pg from "pg";
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
 
-const postgre = require("./database");
+import {} from "dotenv/config";
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+});
 
 const app = express();
 const port = 3000;
@@ -22,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 async function checkScams() {
-  const result = await postgre.query("SELECT * FROM scamlist ORDER BY id ASC");
+  const result = await pool.query("SELECT * FROM scamlist ORDER BY id ASC");
   let scams = result.rows;
   console.log(result.rows);
   return scams;
@@ -42,7 +46,7 @@ app.post("/newscam", async (req, res) => {
   const description = req.body.newScamDescription;
 
   try {
-    await postgre.query(
+    await pool.query(
       "INSERT INTO scamlist (scam_name, scam_description) VALUES ($1, $2)",
       [title, description]
     );
@@ -58,7 +62,7 @@ app.post("/editScam", async (req, res) => {
   const id = req.body.updatedItemId;
 
   try {
-    await postgre.query(
+    await pool.query(
       "UPDATE scamlist SET scam_name = ($1), scam_description = ($2) WHERE id = $3",
       [title, description, id]
     );
@@ -71,7 +75,7 @@ app.post("/editScam", async (req, res) => {
 app.post("/deleteScam", async (req, res) => {
   const id = req.body.deleteItemId;
   try {
-    await postgre.query("DELETE FROM scamlist WHERE id = $1", [id]);
+    await pool.query("DELETE FROM scamlist WHERE id = $1", [id]);
     res.redirect("/");
   } catch (err) {
     console.log(err);
